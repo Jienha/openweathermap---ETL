03524c20ed9739c7a26379d67f46acda34e39516
Pierpaolo Soranzo <pier.sora@gmail.com>
2026-02-20 01:18:32 +0900

    Testing Data Ingestion

--- code/data_ingestion.py
+++ code/data_ingestion.py
@@ -13,15 +13,18 @@ api_key = input("Enter api-key : ")
 print(city_name)
 N = 0
 data = []
-while N < 5:
-    for city in city_name:
-        api = ApiOpenWeatherMap(api_key=api_key)
-        api_response, api_data = api.get(city_name=city)
+# while N < 5:
+for city in city_name:
+    api = ApiOpenWeatherMap(api_key=api_key)
+    api_response, api_data = api.get(city_name=city)
 
-        parser = OpenWeatherMapParser(api_data)
-        data.append(parser.ingestion())
+    parser = OpenWeatherMapParser(api_data)
+    data.append(parser.ingestion())
     N += 1
-    time.sleep(10)
+    # time.sleep(1)
 
 df = pl.DataFrame(data)
-print(df)
+df.write_csv("nome_file.csv")
+
+
+
